d02f24b4a68d4e34575a5d0518d70ca1196492b3
kbs21c1987 <kbs21c1987@gmail.com>
2025-01-14 21:18:31 +0900

    feat: 스크롤바 있는것 없는것 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -5,18 +6,24 @@ import { WebView } from "react-native-webview";
 
 export default function Index() {
   return (
+    // <>
+    //   <StatusBar style="dark" />
+    //   <WebView
+    //     source={{ uri: "" }}
+    //     style={styles.webview}
+    //   />
+    // </>
     <SafeAreaView style={styles.container}>
-      <WebView
-        source={{ uri: process.env.WEBVIEW_URL }}
-        style={styles.webview}
-      />
+      <StatusBar style="dark" />
+      <WebView source={{ uri: "" }} style={styles.webview} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#000",
+    backgroundColor: "#fff",
+    color: "#000",
     flex: 1,
   },
 
